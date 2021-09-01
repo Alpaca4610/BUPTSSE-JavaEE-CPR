ec23158872e3d3b332cdf4a7e2b37ecc8ed99475
alpaca <461065836@qq.com>
2021-09-01 09:21:19 +0800

    增加JPA连接数据库样例

--- src/main/java/com/buptcpr/demo/entity/Student.java
+++ src/main/java/com/buptcpr/demo/entity/Student.java
@@ -1,6 +1,10 @@
 package com.buptcpr.demo.entity;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -10,6 +14,7 @@ public class Student {
 
     //账号信息
     @NotBlank
+    @Id
     private String username;
     @NotBlank
     private String passwd;
