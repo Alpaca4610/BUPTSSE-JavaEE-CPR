e5c07653c7dceee1e681d0fdd71162d37d044c50
pluckpeng <pluckpeng@gmail.com>
2021-09-03 15:39:12 +0800

    整合mvcconfig

--- src/main/java/com/buptcpr/demo/common/WebMvcConfig.java
+++ src/main/java/com/buptcpr/demo/common/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package com.buptcpr.demo.common;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +14,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(new Interceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns("/js/**", "/css/**", "/img/**", "/user/login", "/user/register", "/login.html", "/register.html");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        WebMvcConfigurer.super.addViewControllers(registry);
     }
 }
