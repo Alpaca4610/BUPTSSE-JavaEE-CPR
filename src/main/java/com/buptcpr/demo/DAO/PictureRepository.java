a9f2f02b4aa71201472322f1ba85039d80a503f3
tabby <909702746@qq.com>
2021-09-03 15:33:05 +0800

    picture

---
+++ src/main/java/com/buptcpr/demo/DAO/PictureRepository.java
@@ -0,0 +1,12 @@
+package com.buptcpr.demo.DAO;
+
+
+import com.buptcpr.demo.entity.Picture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PictureRepository extends JpaRepository<Picture, Integer> {
+
+    Object findByPictureName(String pictureName);
+
+
+}
---
+++ src/main/java/com/buptcpr/demo/controller/PictureController.java
@@ -0,0 +1,66 @@
+package com.buptcpr.demo.controller;
+
+import com.buptcpr.demo.DAO.PictureRepository;
+import com.buptcpr.demo.common.Result;
+import com.buptcpr.demo.service.MD5Util;
+import com.buptcpr.demo.service.PictureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@CrossOrigin
+@RestController
+@RequestMapping(path="/picture")
+public class PictureController {
+    @Autowired
+    private PictureService pictureService;
+    @Autowired
+    private PictureRepository pictureRepository;
+    @Autowired
+    private MD5Util md5Util;
+
+    //savePicture
+    @RequestMapping(value = "/savePic", method = RequestMethod.GET)
+    public Result queryBaseInfo(@RequestParam MultipartFile file , @RequestParam String pictureName){
+
+        try {
+            pictureService.savePhoto(file, pictureName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.success();
+
+    }
+    //update picture
+    @RequestMapping(value = "/updatePic", method = RequestMethod.GET)
+    public Result updatePic(@RequestParam MultipartFile file , @RequestParam String pictureName){
+
+        try {
+            pictureService.update(file, pictureName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.success();
+
+    }
+
+    //delete picture
+    @RequestMapping(path="/deletePic")
+    public @ResponseBody String pictureDelete(@RequestParam String pictureName)
+    {
+        return pictureService.delete(pictureName);
+    }
+
+
+    //read
+    @RequestMapping(value = "/image",method = RequestMethod.GET)
+    public @ResponseBody byte[] GetImage(@RequestParam String pictureName)throws IOException{
+        byte[] bytes = pictureService.getImage(pictureName);
+        return bytes;
+
+    }
+
+
+}
