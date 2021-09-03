881b06b9a60e2046e86f6242c929b3d8f1950490
alpaca <461065836@qq.com>
2021-09-03 15:31:47 +0800

    增加前端跳转

---
+++ src/main/java/com/buptcpr/demo/common/config/CorsConfig.java
@@ -0,0 +1,21 @@
+//package com.buptcpr.demo.common.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//
+///**
+// * 跨域配置
+// */
+//@Configuration
+//public class CorsConfig extends WebMvcConfigurationSupport {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("127.0.0.1")
+//                .allowedOrigins("*")
+//                .allowCredentials(true)
+//                .allowedMethods("GET", "POST", "DELETE", "PUT")
+//                .maxAge(3600);
+//    }
+//}
+//
