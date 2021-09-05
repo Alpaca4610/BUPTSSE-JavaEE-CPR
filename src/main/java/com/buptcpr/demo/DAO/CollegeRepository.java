243e3e0b096eb385546a1f91ecee975fc20d1168
alpaca <461065836@qq.com>
2021-09-05 13:40:42 +0800

    修复了清除大学志愿人数的接口

--- src/main/java/com/buptcpr/demo/DAO/CollegeRepository.java
+++ src/main/java/com/buptcpr/demo/DAO/CollegeRepository.java
@@ -2,12 +2,15 @@ package com.buptcpr.demo.DAO;
 
 import com.buptcpr.demo.entity.College;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import javax.swing.text.StyledEditorKit;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
 
+@Transactional
 public interface CollegeRepository extends JpaRepository<College, String> {
     List<College> findByCrankLessThan(int srank);
     List<College> findByCrankGreaterThan(int srank);
@@ -15,6 +18,7 @@ public interface CollegeRepository extends JpaRepository<College, String> {
     @Query(nativeQuery = true,value = "select name,count from college")
     List<Map> getAllCollegeName();
 
+    @Modifying
     @Query(nativeQuery = true,value = "update college set count = 0")
     void clearCount();
 
