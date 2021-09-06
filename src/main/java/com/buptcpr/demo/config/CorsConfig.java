55b11b2e506104e69bf72c5d816ea97659535ea7
Miaohu666 <61049023+Miaohu666@users.noreply.github.com>
2021-09-06 09:37:28 +0800

    Update CorsConfig.java

--- src/main/java/com/buptcpr/demo/config/CorsConfig.java
+++ src/main/java/com/buptcpr/demo/config/CorsConfig.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 //package com.buptcpr.demo.config;
 //
 //import org.springframework.context.annotation.Configuration;
@@ -19,3 +20,26 @@
 //    }
 //}
 //
+=======
+package com.buptcpr.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * 跨域配置
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("http://127.0.0.1")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
+}
+
+>>>>>>> Stashed changes
